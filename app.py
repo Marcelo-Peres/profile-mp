88c952a3b53789303cb9a5cb14249b2abb9268ba
Marcelo-Peres <42848350+Marcelo-Peres@users.noreply.github.com>
2023-05-18 01:24:19 -0300

    reajuste

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st, json, requests, os, shutil, pandas as pd
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
 css_file = current_dir / 'styles' / 'main.css'
-resume_file = current_dir / 'assets' / 'CV.pdf'
+# resume_file = current_dir / 'assets' / 'CV.pdf'
 profile_pic = current_dir / 'assets' / 'mp-pic.png'
 pycache_job = current_dir / 'jobs' / '__pycache__'
 pycache_resume = current_dir / 'resume_data' / '__pycache__'
